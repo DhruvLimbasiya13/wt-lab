ae3c50c5f22b9bd31957c61e66e5ccadb9c68069
Dhruv Limbasiya <24031701027@darshan.ac.in>
2025-09-08 10:48:09 +0530

    new lab

--- react_labs/src/App.jsx
+++ react_labs/src/App.jsx
@@ -1,32 +1,37 @@
-import './App.css'
-import Lab_18_1 from './lab-18/Lab_18_1'
-import Lab_18_2 from './lab-18/Lab_18_2'
-import Lab_18_3 from './lab-18/Lab_18_3'
-import Lab_18_4 from './lab-18/Lab_18_4'
-
-function AddToNumbers(props){
-  return (
-    <>
-    <h1>{props.num1+props.num2}</h1>
-    <button on={()=>{
-      alert('clicked')
-    }}></button>
-
-    {props.isDisplay && <h2>secret</h2>}
-    </>
-  )
-}
+import "./App.css";
+import Lab_18_1 from "./lab-18/Lab_18_1";
+import Lab_18_2 from "./lab-18/Lab_18_2";
+import Lab_18_3 from "./lab-18/Lab_18_3";
+import Lab_18_4 from "./lab-18/Lab_18_4";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./lab-20/Home"
+import About from "./lab-20/About"
+import ContactUs from "./lab-20/ContactUs"
+import Layout from "./lab-20/Layout";
 
 function App() {
-
   return (
     <>
-    {/* <Lab_18_1/> */}
-    {/* <Lab_18_2/> */}
-    {/* <Lab_18_3/> */}
-    <Lab_18_4/>
+      {/* <Lab_18_1/> */}
+      {/* <Lab_18_2/> */}
+      {/* <Lab_18_3/> */}
+      {/* <Lab_18_4/> */}
+      //lab 20
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element ={<Layout/>}>
+              <Route index element={<Home/>}/>
+              <Route path='Home' element={<Home/>}/>
+              <Route path='About' element={<About/>}/>
+              <Route path='ContactUs' element={<ContactUs/>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
